14757de78ae331f3925c4f8ea1ca607365ad5d62
Freddie8606 <ing_julianmadrid@hotmail.com>
2020-08-07 23:29:46 -0500

    Nuevo aprendizaje

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Cabecera></Cabecera>
+    <Lista></Lista>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+/*import HelloWorld from './components/HelloWorld.vue'*/
+import Cabecera from './components/Cabecera.vue'
+import Lista from './components/Listas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cabecera,
+    Lista
   }
 }
 </script>
---
+++ src/components/Listas.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <ul class="list-group">
+            <li v-for="(item, index) of arrayOrden" :key="item.id" @click="aumentar(index)"
+            class="list-group-item d-flex justify-content-between align-items-center">
+                {{index}}  {{item.nombre}}
+                <span class="badge badge-primary badge-pill">
+                    {{item.cantidad}}
+                </span>
+            </li>
+        </ul>
+        <button class="btn btn-block btn-danger" @click="reiniciar()">Reiniciar</button>
+    </div>
+</template>
+<script>
+    import {mapState, mapMutations} from 'vuex';
+    export default {
+        name: 'Lista',
+        computed: {
+            ...mapState(['frutas']),
+            arrayOrden(){
+                return this.frutas.sort((a,b)=> b.cantidad - a.cantidad)
+            }
+        },
+        methods:{
+            ...mapMutations(['aumentar', 'reiniciar'])
+        }
+    }
+</script>
+<style scoped>
+
+</style>
